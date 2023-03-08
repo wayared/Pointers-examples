09acb668725bacb02047b60581fa2cc1f40d822c
wayared <jarecast@espol.edu.ec>
2023-03-07 23:32:38 -0800

    Character arrays and pointers part I

--- pointers7.c
+++ pointers7.c
@@ -1,12 +1,19 @@
 // Character arrays and pointers
 #include<stdio.h>
-
+//How to store strings
 int main(){
 	char C[4];
 	C[0] = 'J';
 	C[1] = 'O';
 	C[2] = 'H';
 	C[3] = 'N';
-	printf("%s",C);	
+	printf("%s\n",C);	
+
+	char A[5] = {'J','O','H','N'};
+	printf("Size in bytes = %d\n",sizeof(A));
+	int len = strlen(A);
+	printf("Length = %d\n", len);
+
+// Arrays and pointers are different types that are used in similar ways
 
 }
